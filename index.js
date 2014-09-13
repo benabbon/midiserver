4e1aa2aad2f07cf486c0e74a980d80972392cba8
Giles Lavelle <giles.lavelle@gmail.com>
2014-09-13 03:32:24 -0400

    fix bug

--- index.js
+++ index.js
@@ -5,29 +5,24 @@ var midi = require('midi');
 var help = require('midi-help');
 
 midiOut = new midi.output();
-
-  midiOut.openVirtualPort('cool-port');
-
+midiOut.openVirtualPort('cool-port');
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
 io.sockets.on('connection', function(socket){
-   console.log('a user connected');  
-   io.sockets.on('sound', function(msg){
-      console.log('message: ' + msg);
-      try {
-      midiOut.sendMessage(help.noteOn(msg.note, msg.velocity));
+  console.log('Connected');
+  socket.on('sound', function(msg){
+    msg = JSON.parse(msg);
+    try {
+      midiOut.sendMessage([144, msg.note, msg.velocity]);
     } catch(error) {
       console.log(error);
     }
+  });
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+  console.log('listening on port 3000');
 });
-
-
-
-
